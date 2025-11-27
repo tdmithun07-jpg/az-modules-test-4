b787368727e788aedb2e192b3235b3af87a2bac7
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-11-27 18:36:31 +0530

    commit 22

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,3 +1,5 @@
 module "dev_resource_group" {
   source = "github.com/tdmithun07-jpg/az-modules-test-4//modules/resource-groups"
+  resource_group_name = var.resource_group_name
+  location            = var.location
 }
