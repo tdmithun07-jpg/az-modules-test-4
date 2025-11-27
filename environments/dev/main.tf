e7b79affd712bcaaabbd05e474900b729532585c
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-11-27 18:13:08 +0530

    commit 18

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,5 +1,3 @@
 module "dev_resource_group" {
   source = "github.com/tdmithun07-jpg/az-modules-test-4//modules/resource-groups"
-  resource_group_name = "rg-dev-2411"
-  location            = "eastus"
 }
--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -19,12 +19,3 @@ provider "azurerm" {
   features {}
 }
 
-# provider "azurerm" {
-#   resource_provider_registrations = "none"
-#   subscription_id = "39a4108d-a3d8-45f9-bf37-b6b174631282"
-#   tenant_id = "7061a6d9-50ef-4504-a8fb-ce81363b8737"
-#   client_id = "95756369-90be-4170-a5b5-ac323df392d8"
-#   client_secret = "NlA8Q~LMtP80mDJsosRBmAMmEeGdgaK9imYujcZO"
-#   features { 
-#   }
-# }
