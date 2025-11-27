6d73ec26927eb5f98f67d9e4126f4c98174cd687
tdmithun07-jpg <tdmithun.07@gmail.com>
2025-11-27 18:55:30 +0530

    commit 23

--- modules/resource-groups/variables.tf
+++ modules/resource-groups/variables.tf
@@ -1,10 +1,11 @@
 variable "resource_group_name" {
   description = "name of the rg"
   type = string
+  default = "dev-rg-2711"
 }
 
 variable "location" {
   description = "region of the rg"
   type = string
-
+  default = "East US"
 }
